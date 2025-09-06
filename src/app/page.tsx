ded03e7853f4825b5a1c65b161efcd65c63a1870
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-06 03:57:05 +0100

    add background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(60,100,200,0.3),rgba(255,255,255,0))] min-h-screen w-full ">
+       <div className="pt-10 mx-[10%]">
+        <p className="text-[40px] sm:text-[80px]">Styq</p>
+       </div>
     </div>
   );
 }
