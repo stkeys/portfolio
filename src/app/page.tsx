6e913a0e1abbcd7c51ef5def40b8bbbca7c7da62
Ola-olukotun Mary <maryomowumi76@gmail.com>
2025-09-08 04:26:55 +0100

    install framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
+
 import Nav from "./components/navbar";
 
 export default function Home() {
   return (
+    
     <div className="bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(60,100,200,0.3),rgba(255,255,255,0))] min-h-screen w-full ">
        <div className="pt-10 mx-[10%]">
         <Nav />
+        <Hero />
        </div>
     </div>
   );
